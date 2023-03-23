2feae1e551a405969b91016db150c88e0bda0b53
PetePiao1017 <petepiao1017@gmail.com>
2023-03-23 03:39:31 -0400

    Add MongoDB Server URI

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI:  "mongodb://localhost:27017/mern-auth",
+    mongoURI:  "mongodb+srv://doadmin:fwJ4G5Ru9C3617d2@db-mongodb-nyc1-93823-92da013f.mongo.ondigitalocean.com/admin?tls=true&authSource=admin",
     secretOrKey: "secret"
-  };
+};
