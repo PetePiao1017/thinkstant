a3e437be64768d8a0d4502ab52ba0a10802f2430
PetePiao1017 <petepiao1017@gmail.com>
2023-03-26 21:08:05 -0400

    Build frontend according to figma

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import {Provider} from 'react-redux';
-import store from './store';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./util/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
@@ -15,15 +15,19 @@ import Chatroom from "./components/layout/Chatroom";
 import DragDrop from "./components/chatbox-upload/DragDrop";
 import Payment from "./components/layout/Payment";
 import EmailVerify from "./components/EmailVerify";
-import Footer from './components/layout/Footer';
+import Footer from "./components/layout/Footer";
+import About from "./components/pages/About/About";
+import Policy from "./components/pages/Policy/Policy";
+import Privacy from "./components/pages/Privacy/Privacy";
+import Error from "./components/pages/Error/Error";
+import FAQ from "./components/pages/FAQ/FAQ";
 
 if (localStorage.jwtToken) {
-
   const token = localStorage.jwtToken;
   setAuthToken(token);
   const decoded = jwt_decode(token);
   store.dispatch(setCurrentUser(decoded));
-  const currentTime = Date.now() / 1000; 
+  const currentTime = Date.now() / 1000;
   if (decoded.exp < currentTime) {
     store.dispatch(logoutUser());
     window.location.href = "./login";
@@ -32,23 +36,27 @@ if (localStorage.jwtToken) {
 
 function App() {
   return (
-    <Provider store={store} >
+    <Provider store={store}>
       <Router>
         <div className="App">
-          <Navbar/>
-          <Route path="/" component = {DragDrop} exact />
-          <Route path="/register" component={Register} exact/>
-          <Route path="/login" component={Login} exact/>
+          <Navbar />
+          <Route path="/register" component={Register} exact />
+          <Route path="/login" component={Login} exact />
+          <Route path="/" component={DragDrop} exact />
+          <Route path="/policy" component={Policy} />
+          <Route path="/privacy" component={Privacy} />
+          <Route path="/error" component={Error} />
+          <Route path="/faq" component={FAQ} />
+          <Route path="/about" component={About} exact />
           <Route exact path="/chatroom" component={Chatroom} />
           <Route path="/users/:id/verify/:token" element={<EmailVerify />} />
           <Switch>
-              <PrivateRoute exact path="/payment" component={Payment} />
+            <PrivateRoute exact path="/payment" component={Payment} />
           </Switch>
           <Footer />
         </div>
       </Router>
     </Provider>
-    
   );
 }
 
